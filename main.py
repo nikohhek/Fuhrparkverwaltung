774c2affc3cd27aedf735ff055160faeef1eec87
nikohhek <nspiegelhoff@hhek.de>
2025-10-08 16:09:25 +0200

    changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 class Fahrzeug:
-    def Fahrzeug(self, kennzeichen, hersteller, modell, baujahr):
+    def Fahrzeug(self, kennzeichen: str, hersteller: str, modell: str, baujahr: int):
         self.__kenzeichen = kennzeichen
         self.__hersteller = hersteller
         self.__modell = modell
         self.__baujahr = baujahr
 
 class PKW(Fahrzeug):
-    def PKW(self, kennzeichen, hersteller, modell, baujahr, anzahlTueren):
+    def PKW(self, kennzeichen: str, hersteller: str, modell: str, baujahr: int, anzahlTueren: int):
         self.__kenzeichen = kennzeichen
         self.__hersteller = hersteller
         self.__modell = modell
@@ -14,7 +14,7 @@ class PKW(Fahrzeug):
         self.__anzahlTueren = anzahlTueren
 
 class LKW(Fahrzeug):
-    def LKW(self, kennzeichen, hersteller, modell, baujahr, ladekapazitaetKG):
+    def LKW(self, kennzeichen: str, hersteller: str, modell: str, baujahr: int, ladekapazitaetKG: int):
         self.__kenzeichen = kennzeichen
         self.__hersteller = hersteller
         self.__modell = modell
