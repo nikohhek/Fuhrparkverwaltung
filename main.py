8cac38a003b61e01810b61bfe3c26fb2e51fe4f0
nikohhek <nspiegelhoff@hhek.de>
2025-10-08 16:44:33 +0200

    Changes (Funktioniert nicht)

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 class Fahrzeug:
-    def Fahrzeug(self, kennzeichen: str, hersteller: str, modell: str, baujahr: int):
+    def __init__(self, kennzeichen: str, hersteller: str, modell: str, baujahr: int):
         self.__kenzeichen = kennzeichen
         self.__hersteller = hersteller
         self.__modell = modell
         self.__baujahr = baujahr
 
+    def getKennzeichen() -> str:
+        return __kennzeichen
+
 class PKW(Fahrzeug):
-    def PKW(self, kennzeichen: str, hersteller: str, modell: str, baujahr: int, anzahlTueren: int):
+    def __init__(self, kennzeichen: str, hersteller: str, modell: str, baujahr: int, anzahlTueren: int):
         self.__kenzeichen = kennzeichen
         self.__hersteller = hersteller
         self.__modell = modell
@@ -14,9 +17,15 @@ class PKW(Fahrzeug):
         self.__anzahlTueren = anzahlTueren
 
 class LKW(Fahrzeug):
-    def LKW(self, kennzeichen: str, hersteller: str, modell: str, baujahr: int, ladekapazitaetKG: int):
+    def __init__(self, kennzeichen: str, hersteller: str, modell: str, baujahr: int, ladekapazitaetKG: int):
         self.__kenzeichen = kennzeichen
         self.__hersteller = hersteller
         self.__modell = modell
         self.__baujahr = baujahr
         self.__ladekapazitaetKG = ladekapazitaetKG
+
+def main():
+    Seat = Fahrzeug("SU-N-9513", "Hersteller", "Leon", 2002)
+    Seat.getKennzeichen()
+
+main()
