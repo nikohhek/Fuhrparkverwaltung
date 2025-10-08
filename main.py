031037d8f51d7d0519c54223580e0f0193662b0f
nikohhek <nspiegelhoff@hhek.de>
2025-10-08 15:59:35 +0200

    Einfache Fahrzeugklassen

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 class Fahrzeug:
-    def Fahrzeug(self, )
+    def Fahrzeug(self, kennzeichen, hersteller, modell, baujahr):
+        self.__kenzeichen = kennzeichen
+        self.__hersteller = hersteller
+        self.__modell = modell
+        self.__baujahr = baujahr
+
+class PKW(Fahrzeug):
+    def PKW(self, kennzeichen, hersteller, modell, baujahr, anzahlTueren):
+        self.__kenzeichen = kennzeichen
+        self.__hersteller = hersteller
+        self.__modell = modell
+        self.__baujahr = baujahr
+        self.__anzahlTueren = anzahlTueren
+
+class LKW(Fahrzeug):
+    def LKW(self, kennzeichen, hersteller, modell, baujahr, ladekapazitaetKG):
+        self.__kenzeichen = kennzeichen
+        self.__hersteller = hersteller
+        self.__modell = modell
+        self.__baujahr = baujahr
+        self.__ladekapazitaetKG = ladekapazitaetKG
