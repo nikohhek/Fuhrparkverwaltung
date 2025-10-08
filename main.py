df9b2203ff0b1f58a8f9cd2def7e3332b51757bf
nikohhek <nspiegelhoff@hhek.de>
2025-10-08 16:50:32 +0200

    Bugfix: - Rechtschreibfehler - get-Function nicht richtig geschrieben

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 class Fahrzeug:
     def __init__(self, kennzeichen: str, hersteller: str, modell: str, baujahr: int):
-        self.__kenzeichen = kennzeichen
+        self.__kennzeichen = kennzeichen
         self.__hersteller = hersteller
         self.__modell = modell
         self.__baujahr = baujahr
 
-    def getKennzeichen() -> str:
-        return __kennzeichen
+    def getKennzeichen(self) -> str:
+        return self.__kennzeichen
 
 class PKW(Fahrzeug):
     def __init__(self, kennzeichen: str, hersteller: str, modell: str, baujahr: int, anzahlTueren: int):
@@ -26,6 +26,6 @@ class LKW(Fahrzeug):
 
 def main():
     Seat = Fahrzeug("SU-N-9513", "Hersteller", "Leon", 2002)
-    Seat.getKennzeichen()
+    print(Seat.getKennzeichen())
 
 main()
