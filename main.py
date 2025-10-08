8a48d4c2d19e70f6eaa58e91a875772ecce9d5ea
nikohhek <nspiegelhoff@hhek.de>
2025-10-08 17:00:14 +0200

    Basisfunktionen fertig

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ class Fahrzeug:
     def getKennzeichen(self) -> str:
         return self.__kennzeichen
 
+    def getHersteller(self) -> str:
+        return self.__hersteller
+
+    def getModell(self) -> str:
+        return self.__modell
+
+    def getBaujahr(self) -> int:
+        return self.__baujahr
+
+
 class PKW(Fahrzeug):
     def __init__(self, kennzeichen: str, hersteller: str, modell: str, baujahr: int, anzahlTueren: int):
         self.__kenzeichen = kennzeichen
@@ -15,6 +25,10 @@ class PKW(Fahrzeug):
         self.__modell = modell
         self.__baujahr = baujahr
         self.__anzahlTueren = anzahlTueren
+
+    def getAnzahlTueren(self) -> str:
+        return self.__anzahlTueren
+
 
 class LKW(Fahrzeug):
     def __init__(self, kennzeichen: str, hersteller: str, modell: str, baujahr: int, ladekapazitaetKG: int):
@@ -24,8 +38,13 @@ class LKW(Fahrzeug):
         self.__baujahr = baujahr
         self.__ladekapazitaetKG = ladekapazitaetKG
 
+    def getLadekapazitaetKG(self) -> int:
+        return self.__ladekapazitaetKG
+
+
 def main():
     Seat = Fahrzeug("SU-N-9513", "Hersteller", "Leon", 2002)
     print(Seat.getKennzeichen())
+
 
 main()
