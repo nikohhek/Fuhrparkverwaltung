4f88be003ae9019c4853da3219723bb7054f2605
nikohhek <nspiegelhoff@hhek.de>
2025-10-29 14:40:07 +0100

    Frontend angefangen

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from modules import fuhrpark
 
+# Text User Interface
+def tui(Fuhrpark):
+    druckeFahrzeuge(Fuhrpark)
+
+def druckeFahrzeuge(Fuhrpark):
+    fahrzeugListe = Fuhrpark.getFahrzeuge()
+    for kategorie in fahrzeugListe:
+        print(f"{20*"="}\n{kategorie}\n{20*"="}\n\n")
+        for fahrzeug in kategorie:
+            print(f"{fahrzeugListe}")
+
 def main():
     # Initiierung von Fuhrpark-Objekt zur Verwaltung
-    TestFuhrpark = fuhrpark.Fuhrpark([])
-
+    Fuhrpark = fuhrpark.Fuhrpark([])
     # manuelle Tests
-    TestFuhrpark.addFahrzeug("PKW", "SU_N_9513", "Seat", "Leon", 2002, 5, 0)
-    TestFuhrpark.addFahrzeug("LKW", "SU_SJ_513", "Fiat", "Fullback", 2017, 0, 500)
-    TestFuhrpark.addFahrzeug("PKW", "SU_N_9513", "Merc", "GLB", 2022, 5, 0)
-    print(TestFuhrpark.getFahrzeuge())
+    Fuhrpark.addFahrzeug("PKW", "SU_N_9513", "Seat", "Leon", 2002, 5, 0)
+    Fuhrpark.addFahrzeug("LKW", "SU_SJ_513", "Fiat", "Fullback", 2017, 0, 500)
+    Fuhrpark.addFahrzeug("PKW", "SU_N_9513", "Merc", "GLB", 2022, 5, 0)
+    print(Fuhrpark.getFahrzeuge())
+    tui(Fuhrpark)
 
 if __name__ == "__main__":
     main()
